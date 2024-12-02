80d1b2ec26709b942ff922407afa6330e5125fb3
Rollen Ricafort <rollenricafort011@gmail.com>
2024-12-02 09:34:50 +0800

    disable-eslint-onbuild

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -61,6 +61,7 @@ export async function createInvoice(prevState: State, formData: FormData) {
       VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
       `;
     } catch (error) {
+      console.log(error);
       return {
         message: 'Database Error: Failed to Create Invoice',
       };
@@ -102,6 +103,7 @@ export async function deleteInvoice(id: string) {
         revalidatePath('/dashboard/invoices');
         return { message: 'Deleted Invoice' };
     } catch (error) {
+      console.log(error);
         return {
             message: 'Database Error: Failed to Create Invoice.',
         };
